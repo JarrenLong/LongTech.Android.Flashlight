be6cf896569d1a386f8b538b2f9cdc99033b1e62
Jarren Long <jarrenlong@gmail.com>
2019-01-28 10:38:52 -0800

    Initial commit

---
+++ FlashlightService.cs
@@ -0,0 +1,57 @@
+﻿using Android.Content;
+using Android.Hardware;
+using Android.Hardware.Camera2;
+using Android.OS;
+
+namespace LongTech.Android.Flashlight
+{
+  /// <summary>
+  /// Provides generic service for toggling the flashlight's state
+  /// </summary>
+  public class FlashlightService
+  {
+#pragma warning disable CS0618
+    private static bool FlashLightOn = false;
+    private static Camera mCamera;
+    private static CameraManager camManager;
+
+    /// <summary>
+    /// Turns the camera's flashlight on or off if supported
+    /// </summary>
+    /// <param name="enable"></param>
+    public static void ToggleFlashlight(Context ctx)
+    {
+      FlashLightOn = !FlashLightOn;
+
+      try
+      {
+        if (Build.VERSION.SdkInt >= Build.VERSION_CODES.M)
+        {
+          if (camManager == null)
+            camManager = (CameraManager)ctx.GetSystemService(Context.CameraService);
+          string cameraId = null; // Usually back camera is at 0 position.
+          try
+          {
+            cameraId = camManager.GetCameraIdList()[0];
+            camManager.SetTorchMode(cameraId, FlashLightOn);
+          }
+          catch { }
+        }
+        else
+        {
+          if (mCamera == null)
+            mCamera = Camera.Open();
+          var parameters = mCamera.GetParameters();
+          parameters.FlashMode = FlashLightOn ? Camera.Parameters.FlashModeTorch : Camera.Parameters.FlashModeOff;
+          mCamera.SetParameters(parameters);
+          if (FlashLightOn)
+            mCamera.StartPreview();
+          else
+            mCamera.StopPreview();
+        }
+      }
+      catch { }
+    }
+#pragma warning restore CS0618
+  }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,86 @@
+﻿using Android.App;
+using Android.Gms.Ads;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace LongTech.Android.Flashlight
+{
+  [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+  public class MainActivity : AppCompatActivity
+  {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+      base.OnCreate(savedInstanceState);
+      SetContentView(Resource.Layout.activity_main);
+
+      MobileAds.Initialize(this, GetString(Resource.String.flashlightAdAppId));
+
+      mAdView = FindViewById<AdView>(Resource.Id.adView);
+      mAdView.AdListener = new MyAdListener();
+      var adRequest = new AdRequest.Builder().Build();
+      mAdView.LoadAd(adRequest);
+
+      RelativeLayout fab = FindViewById<RelativeLayout>(Resource.Id.toggleFlashlight);
+      fab.Click += (s, e) => { FlashlightService.ToggleFlashlight(this); };
+    }
+
+    protected AdView mAdView;
+
+    protected override void OnPause()
+    {
+      if (mAdView != null)
+        mAdView.Pause();
+
+      base.OnPause();
+    }
+
+    protected override void OnResume()
+    {
+      base.OnResume();
+
+      if (mAdView != null)
+        mAdView.Resume();
+    }
+
+    protected override void OnDestroy()
+    {
+      if (mAdView != null)
+        mAdView.Destroy();
+
+      base.OnDestroy();
+    }
+
+    class MyAdListener : AdListener
+    {
+      public override void OnAdClicked()
+      {
+        base.OnAdClicked();
+      }
+      public override void OnAdClosed()
+      {
+        base.OnAdClosed();
+      }
+      public override void OnAdFailedToLoad(int errorCode)
+      {
+        base.OnAdFailedToLoad(errorCode);
+      }
+      public override void OnAdImpression()
+      {
+        base.OnAdImpression();
+      }
+      public override void OnAdLeftApplication()
+      {
+        base.OnAdLeftApplication();
+      }
+      public override void OnAdLoaded()
+      {
+        base.OnAdLoaded();
+      }
+      public override void OnAdOpened()
+      {
+        base.OnAdOpened();
+      }
+    }
+  }
+}
